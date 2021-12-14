3b5389552f4e865fdb5ef9f404c7665f0fbc2edb
Kira <mukeshreddy.9.new@gmail.com>
2021-12-14 14:58:58 +0530

    [feat] initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ASEAN BTE',
+  tagline: 'We are building southeast asia in minecraft!',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/aseanbte_logo.png',
+  organizationName: 'ASEAN-Build-The-Earth',
+  projectName: 'ASEAN-Docs',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: '',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            '',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'ASEAN BTE',
+        logo: {
+          alt: 'ASEAN BTE',
+          src: 'img/aseanbte_logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/ASEAN-Build-The-Earth/ASEAN-Docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'How to join ASEAN BTE',
+                to: '/docs/how-to-join',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/cHCwd7BWdZ',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/AseanBte',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ASEAN-Build-The-Earth',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
