dc94b2254d7690c9fb115342da8a4c0bdd7d364e
Tintinkung <sukrit_dinosuar@hotmail.com>
2022-02-16 00:20:53 +0700

    [feat] util folder for storing re-usable react component

---
+++ util/styling.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import clsx from 'clsx';
+
+/* Highlight component use for hightligting text's background 
+    ```html
+    <Highlight color="#25c2a0" textColor="#fff" >Docusaurus green</Highlight>
+    ```
+*/
+export default function Highlight ({children, color, textColor}) 
+{
+    return <span
+      style={{
+        backgroundColor: color? color : null,
+        borderRadius: '2px',
+        color: textColor? textColor : '#fff',
+        padding: '0.2rem',
+      }}>
+      {children}
+    </span>
+}
+  
