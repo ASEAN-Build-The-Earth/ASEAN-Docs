215570580243d381256b5d2b41301746b0103f20
Tintinkung <sukrit_dinosuar@hotmail.com>
2022-02-26 22:43:05 +0700

    [Edit] get-started.md
    *its now .mdx

--- util/styling.jsx
+++ util/styling.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import clsx from 'clsx';
 
 /* Highlight component use for hightligting text's background 
-    ```html
-    <Highlight color="#25c2a0" textColor="#fff" >Docusaurus green</Highlight>
-    ```
+  ```html
+  <Highlight color="#25c2a0" textColor="#fff" >Docusaurus green</Highlight>
+  ```
 */
 export default function Highlight ({children, color, textColor}) 
 {
-    return <span
-      style={{
-        backgroundColor: color? color : null,
-        borderRadius: '2px',
-        color: textColor? textColor : '#fff',
-        padding: '0.2rem',
-      }}>
-      {children}
-    </span>
+  return <span
+    style={{
+      backgroundColor: color? color : null,
+      borderRadius: '2px',
+      color: textColor? textColor : '#fff',
+      padding: '0.2rem',
+    }}>
+    {children}
+  </span>
 }
-  
