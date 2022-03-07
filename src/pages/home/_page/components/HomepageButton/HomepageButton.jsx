bf18eaaf5545fc6a713f8d711daa68f1e897f5a0
Tintinkung <sukrit_dinosuar@hotmail.com>
2022-03-08 05:18:41 +0700

    [Feat] Revamp Homepage route and style
    *Revamp `@site/src/Homepage` hierarchy to fit commit [b0365867fbf9272aea3de5b846d03f0c6f814a25]
    
    *Re-organized homepage buttons and style
    
    *Also added new homepage banner image
    
    <tintinkung.lemonade@gmail.com>

---
+++ src/pages/home/_page/components/HomepageButton/HomepageButton.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageButton.module.css'
+
+import Translate, { translate } from '@docusaurus/Translate';;
+import Link from '@docusaurus/Link';
+import Image from '@theme/IdealImage';
+import book_icon from "@site/static/img/icons/book_1.png"
+import kofi_icon from "@site/static/img/icons/kofi_logo.png"
+import feather_icon from "@site/static/img/icons/feather.png"
+import kofi_pixel_icon from "@site/static/img/icons/kofi_pixel_logo.png";
+
+const ButtonContents = [
+    {
+        "id" : "donate_button",
+        "component": <>
+            <div className={styles.button_div} style={{transform:"scaleX(-1)"}}>
+                <Link to="support" className={clsx("button button--link", styles.button_container)}>
+                    <nav aria-label="breadcrumbs">
+                        <ul className={clsx("breadcrumbs breadcrumbs--lg", styles.breadcrumbs_cover)}>
+                            <li class="breadcrumbs__item">
+                                <a class="breadcrumbs__link" style={{transform:"scaleX(-1)"}}>
+                                    <Translate descripton="The big Start building button (please keep the text count very low to prevent newline)">
+                                        Support us
+                                    </Translate>     
+                                </a>
+                            </li>
+                            <Image img={kofi_icon} alt="book" id={styles.button_icon} style={{backgroundColor: "#13C3FF", padding:"0px"}}/>
+                        </ul>
+                    </nav>
+                </Link> 
+                <ButtonOverlay mirror="true" color="#FF5E5B" icon={<Image img={kofi_pixel_icon} alt="↗" style={{width:"100%", position:"absolute", transform:"translateY(1rem)"}} />}>
+                    <span className={styles.button_overlay_text_support} >
+                        <Translate descripton="The pop up message when you hover to the homepage Donate us button (please keep the text count for this low)">
+                            Donate on kofi
+                        </Translate>
+                    </span>
+                </ButtonOverlay>
+            </div>
+        </>
+    },
+    {
+        "id" : "get_started_button",
+        "component": <>
+            <div className={styles.button_div}>
+                <Link to="intro/get-started" className={clsx("button button--link", styles.button_container)}>
+                    <nav aria-label="breadcrumbs">
+                        <ul className={clsx("breadcrumbs breadcrumbs--lg", styles.breadcrumbs_cover)}>
+                            <li class="breadcrumbs__item">
+                                <a class="breadcrumbs__link">
+                                    <Translate descripton="The big Start building button (please keep the text count very low to prevent newline)">
+                                        Start Building
+                                    </Translate>     
+                                </a>
+                            </li>
+                            <Image img={book_icon} alt="book" id={styles.button_icon} />
+                        </ul>
+                    </nav>
+                </Link> 
+                <ButtonOverlay icon={<Image img={feather_icon} alt="↗" style={{width:"100%", position:"absolute", imageRendering: "pixelated"}} />}>
+                    <span className={styles.button_overlay_text_getstart} >
+                        <Translate descripton="The pop up message when you hover to the homepage Start Building button (please keep the text count for this low)">
+                            Go To Document
+                        </Translate>
+                    </span>
+                </ButtonOverlay>
+            </div>
+        </>
+    }
+]
+
+
+function ButtonArrow ({color, transform}) {
+    return <>
+        <svg 
+            width="300%" 
+            height="100%" 
+            style={{
+                fill:"none", 
+                stroke: ( typeof color === "string" ) ? color : "var(--ifm-color-primary)" , 
+                strokeWidth:"40",
+                transform: ( typeof transform === "string" ) ? `translateX(${transform})` : "translateX(0)",
+            }}
+        >   <polyline points="-150,-50 -45,55 -150,150"/>
+            <polyline points=" 50,-50 155,55 50,150  " />
+            <polyline points="150,-50 255,55 150,150 " />
+            <polyline points="250,-50 355,55 250,150 " />
+            <polyline points="350,-50 455,55 350,150 " />
+            <polyline points="450,-50 555,55 450,150 " />
+            <polyline points="550,-50 655,55 550,150 " />
+            <polyline points="650,-50 755,55 650,150 " />
+            <polyline points="750,-50 855,55 750,150 " />
+            <polyline points="850,-50 955,55 850,150 " />
+            <polyline points="950,-50 1055,55 950,150" />
+        </svg>
+    </>
+}
+
+function ButtonOverlay ({color, children, icon, mirror}) {
+    return (
+        <div className={styles.buttons_overlay}> 
+            <div className={styles.arrow_div 
+            /* main div that cover arrow loop thing 
+            * also hard coded the transform pixel so it loop perfectly
+            */
+            }>  <div className={clsx(styles.arrow_1)}>
+                    <ButtonArrow transform="-200px" color={color}/>
+                </div>
+                <div className={clsx(styles.arrow_2, styles.arrow_1)}>  
+                    <ButtonArrow transform="-100px" color={color}/>
+                </div> 
+                <div className={clsx(styles.arrow_2, styles.arrow_1)}>  
+                    <ButtonArrow transform="500px" color={color}/>
+                </div>       
+            </div>
+            {
+                mirror === "true" ? (
+                    <div className={styles.button_overlay_div}>
+                        <div style={{transform:"scaleX(-1)"}}>
+                        <p><strong>{children}</strong></p>
+                        </div>
+                        <div className={styles.button_overlay_icon}>{icon}</div>
+                    </div>  
+                ) : (
+                    <div className={styles.button_overlay_div}>
+                        <p><strong>{children}</strong></p>
+                        <div className={styles.button_overlay_icon}>{icon}</div>
+                    </div>  
+                )
+            }
+              
+        </div>
+    )
+}
+
+const ButtonHeader = () => {
+    return (
+        <>
+            {
+                ButtonContents.map((item) => ( /* loop thru drop down items */
+                        <div className={clsx('col col--6', styles.contents_column)}>
+                            <div className="text--center padding-horiz--md">
+                                <div key={item.id}>
+                                    {item.component}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                )
+            }
+        </>
+    );
+}
+
+export default ButtonHeader;
---
+++ src/pages/home/_page/homepage.jsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './homepage.module.css';
+
+import Translate, { translate } from '@docusaurus/Translate';
+import CodeBlock from '@theme/CodeBlock';
+import Link from '@docusaurus/Link';
+import Admonition from '@theme/Admonition';
+import Details from '@theme/Details'
+import Image from '@theme/IdealImage';
+import ButtonHeader from "./components/HomepageButton"
+
+import text from "@site/src/components/TextDecoration"
+import grassblock_icon from "@site/static/img/icons/dancing_grassblock.gif"
+
+
+const ContentList = [
+  {
+    title: <Translate>ASEAN BuildTheEarth</Translate>,
+    description: (
+      <>
+        <br/>
+        <Translate>
+          We are The Association of Southeast Asian Nations Build the Earth. 
+          We are part of the build the earth project which aims to build the world 1:1 scale in Minecraft! 
+          We currently oversee the progress of Brunei, Myanmar, 
+          Cambodia, Timor-Leste, Indonesia, Laos, Malaysia, the Philippines, 
+          Singapore, Thailand, and Vietnam.
+        </Translate>
+        <br/>
+        <hr/> 
+      </>
+    ),
+  },
+  {
+    title: <><img src={grassblock_icon} alt="Our Server" style={{ filter: "drop-shadow(1px 1px 1px #222)"}} /></>,
+    description: (
+      <>
+        {/* Server IP and Requirement box */}
+        <div className={styles.container}>
+          <div className={styles.ip_div}>
+            <table className={styles.ip_box} ><td id={styles.box_styles}>
+              <div align="Left"><i><b><Translate>Server ip</Translate></b></i></div>
+              <sub><CodeBlock className="language-js" title="java">
+                139.99.91.188:25569
+              </CodeBlock>
+              <CodeBlock className="language-js" title="bedrock">
+                IP: 139.99.91.188 
+                PORT: 19132
+              </CodeBlock></sub>
+            </td></table>
+          </div>
+          <div className={styles.req_div} >
+            <table className={styles.req_box}><td id={styles.box_styles}>
+              <div align="Left"><i><b><sub><Translate>Requirements</Translate></sub></b></i></div>
+              <div align="Left">
+                <Details summary={<summary><Translate>Minecraft</Translate></summary>}>
+                  <sub style={{ lineHeight: 1}}>
+                    <a href="https://www.minecraft.net/en-us/store/minecraft-java-edition">
+                      <Translate descripton="a section of 'Java Edition and Bedrock Edition is supported'">
+                        Java Edition
+                      </Translate>
+                    </a> <Translate descripton="a section of 'Java Edition and Bedrock Edition is supported'">
+                        and
+                      </Translate> <a href="https://minecraft.fandom.com/wiki/Bedrock_Edition">
+                      <Translate descripton="section of 'Java Edition and Bedrock Edition is supported'">
+                        Bedrock Edition
+                      </Translate>
+                    </a> <Translate descripton="section of 'Java Edition and Bedrock Edition is supported'">
+                      is supported
+                    </Translate>
+                  </sub>
+                </Details>
+              </div>
+              <div align="Left">
+                <Admonition type="caution">
+                  <sub>
+                    <text.Hl color="#C03A3A" textColor="#FEF6E8">
+                      <Translate descripton="section of 'Cracked Minecraft is not allowed'">
+                        Cracked Minecraft
+                      </Translate>
+                    </text.Hl> <Translate descripton="section of 'Cracked Minecraft is not allowed'">is not allowed</Translate>
+                  </sub>
+                </Admonition>
+              </div>
+            </td></table>
+          </div>  
+        </div>
+      </>
+    ),
+  }
+];
+
+function Content({ title, description }) {
+  return (
+    <div className={clsx('col col--6')}>
+      <div className="text--center padding-horiz--md">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function Contents() {
+  return ( <>  
+    <section className={styles.contents}>
+    <div className="container">
+        <div className="row" >
+          {ContentList.map((props, idx) => (
+            <Content key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  </>);
+}
+
+function Header() {
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        {/*this title is based on {siteConfig.title & tagline} but exports raw to use Translate tag */}
+        <h1 className={clsx('hero__title', styles.title, styles.shadowed)}>     
+          <Translate description="A hero title of homepage document">
+            ASEAN BTE
+          </Translate>
+        </h1>
+        <p className={clsx("hero__subtitle", styles.title, styles.shadowed)}>
+          <Translate description="A hero description of homepage document">
+            We are building southeast asia in Minecraft 1:1 scale!
+          </Translate>
+        </p>
+        <div className={styles.intro_button}>
+          <section className={clsx(styles.intro_button_contents)}>
+          <div className="container">
+            <div className={clsx("row", styles.reverse_row)} >
+              <ButtonHeader />
+            </div>
+          </div>
+        </section>
+        </div>
+        
+      </div>
+    </header>
+  );
+}
+
+export default { Contents, Header }
