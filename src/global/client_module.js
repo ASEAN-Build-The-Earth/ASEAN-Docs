886c225042c011535dbb53f3e3868663871717c5
Tintinkung <sukrit_dinosuar@hotmail.com>
2022-03-22 04:49:47 +0700

    [Fix] font priority
    <tintinkung.lemonade@gmail.com>

--- src/global/client_module.js
+++ src/global/client_module.js
@@ -12,13 +12,20 @@ if (ExecutionEnvironment.canUseDOM) {
         + document.baseURI.charAt(document.baseURI.length - 2) 
         + document.baseURI.charAt(document.baseURI.length - 1);
 
+    console.log("entered locale of ", locale);
+
     if(locale === "th/" || locale === "/th") 
     {  /* append a google font to every thai locale*/
         var font = document.createElement("link");
         font.rel = "stylesheet";
         font.href = "https://fonts.googleapis.com/css?family=Noto+Sans+Thai";
-        font.type = "text/css";
+        // font.type = "text/css";
         document.head.appendChild(font);
+        console.log("appended script child");
+    }
+    else
+    {
+        console.log("no thai locale found");
     }
 }
 
