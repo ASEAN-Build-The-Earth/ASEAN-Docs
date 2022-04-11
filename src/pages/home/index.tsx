be0c493998c72f8bb14d012c913fef4772ec3e2c
Tintinkung <tintinkung.lemonade@gmail.com>
2022-04-11 19:24:13 +0700

    [Typo] forgor to switch old path
    <tintinkung.lemonade@gmail.com>

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,9 +11,9 @@ export default function ToHomepage(): JSX.Element {
       <meta title="ASEAN BTE" />
       <meta property="og:title" content="ASEAN BTE" />
       <meta name="theme-color" content="#ffc561" />
-      <meta data-rh="true" property="og:image" content={`${siteConfig.url}/img/icons/aseanbte_full_logo.jpg`} />
+      <meta data-rh="true" property="og:image" content={`${siteConfig.url}/media/icons/aseanbte_full_logo.jpg`} />
       <meta name="twitter:card" content="summary_large_image" data-rh="true" />
-      <meta data-rh="true" name="twitter:image" content={`${siteConfig.url}/img/icons/aseanbte_full_logo.jpg`} />
+      <meta data-rh="true" name="twitter:image" content={`${siteConfig.url}/media/icons/aseanbte_full_logo.jpg`} />
       <meta property="og:description" content={siteConfig.tagline} />
       <meta property="description" content={siteConfig.tagline} />
       <meta property="og:url" content={siteConfig.url} />
